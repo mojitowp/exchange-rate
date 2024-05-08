02bf69a8ee408e3a9205780ddd63672acd41fc2b
Manfred <rofamabu@gmail.com>
2024-05-08 08:32:02 -0600

    chore: Update Hacienda provider to use cURL for fetching rates

--- src/providers/cr/class-hacienda.php
+++ src/providers/cr/class-hacienda.php
@@ -10,6 +10,27 @@ class Hacienda extends Provider{
     }
 
     public function getRates(){
-        return json_decode( file_get_contents( $this->url ) );
+        
+        // Inicializar sesión cURL
+        $ch = curl_init($this->url);
+
+        // Configurar opciones de cURL
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Para que curl_exec() devuelva el resultado de la transferencia como string
+        curl_setopt($ch, CURLOPT_HEADER, false); // No incluir los headers en el resultado
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Wget/1.21.2 (linux-gnu)'); // Simular User-Agent de wget
+
+        // Ejecutar sesión cURL
+        $response = curl_exec($ch);
+
+        // Verificar si ocurrió algún error durante la ejecución
+        if(curl_errno($ch)){
+            error_log('Error en cURL: ' . curl_error($ch));
+            return false;
+        }
+
+        // Cerrar sesión cURL
+        curl_close($ch);
+
+        return json_decode( $response );
     }
 }
